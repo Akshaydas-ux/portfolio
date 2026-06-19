b79f76a1dbe362651c807c87581f05469402c637
Akshaydas-ux <achuakshaym2006@gmail.com>
2026-06-19 20:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach(section => {
+
+const sectionTop = section.offsetTop - 150;
+
+if(scrollY >= sectionTop){
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach(link => {
+
+link.classList.remove("active");
+
+if(link.getAttribute("href").includes(current)){
+link.classList.add("active");
+}
+
+});
+
+});
+
 const reveals = document.querySelectorAll('.fade-up');
 
 function revealSections() {
