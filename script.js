451c2c9b44f129a0505e960cf03e893212321260
Akshaydas-ux <achuakshaym2006@gmail.com>
2026-06-19 20:19:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left = e.clientX + "px";
+glow.style.top = e.clientY + "px";
+
+});
+
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
 
