05739a6741b469fc3cd950a0568de7b1c4fb8dce
Akshaydas-ux <achuakshaym2006@gmail.com>
2026-06-26 11:40:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,31 @@ function hideLoader() {
 window.addEventListener("load", hideLoader);
 
 setTimeout(hideLoader, 3000);
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+document.querySelectorAll(".project-card img").forEach(img => {
+
+img.addEventListener("click", () => {
+
+modal.style.display = "flex";
+modalImg.src = img.src;
+
+});
+
+});
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+
+modal.style.display = "none";
+
+});
+
+modal.addEventListener("click",(e)=>{
+
+if(e.target === modal){
+modal.style.display = "none";
+}
+
+});
