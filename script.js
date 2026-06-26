39c526b0b6513f4a09924c6fda182385cad629c7
Akshaydas-ux <achuakshaym2006@gmail.com>
2026-06-26 12:02:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -143,3 +143,31 @@ modal.style.display = "none";
 }
 
 });
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>500){
+
+scrollBtn.style.display="block";
+
+}else{
+
+scrollBtn.style.display="none";
+
+}
+
+});
+
+scrollBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
